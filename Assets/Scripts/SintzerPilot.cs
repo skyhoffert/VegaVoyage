ca6564e657bcb6e73f591b6937c56fac7b86bc11
skyhoffert <skwylarh@yahoo.com>
2019-01-05 22:35:23 -0500

    Basic AI in progress for Sintzer.

--- Assets/Scripts/SintzerPilot.cs
+++ Assets/Scripts/SintzerPilot.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class SintzerPilot : MonoBehaviour
 {
+
+    private Vector2 forward;
+    private float rotation_speed = 5.0f;
+
+    private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        float y_dist = player.transform.position.y - this.transform.position.y;
+        float x_dist = player.transform.position.x - this.transform.position.x;
+
+        // calculate forward direction
+        forward.x = Mathf.Sin(this.transform.rotation.eulerAngles.z * Mathf.Deg2Rad + Mathf.PI/2);
+        forward.y = -Mathf.Cos(this.transform.rotation.eulerAngles.z * Mathf.Deg2Rad + Mathf.PI/2);
+        forward.Normalize();
         
+        // calculate how much to turn
+        float rotation_amount = Mathf.Atan2(y_dist, x_dist) * Mathf.Rad2Deg + Mathf.PI/2;
+        
+        // Lerp towards that direction
+        Vector3 dir = new Vector3(0.0f, 0.0f, rotation_amount);
+        Quaternion target = Quaternion.Euler(dir);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, target, Time.deltaTime * this.rotation_speed);
+
+        Debug.Log(this.forward);
     }
 }

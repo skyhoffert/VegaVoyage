8234a44c01a7d732e32f40f3747f79259f4a223c
skyhoffert <skwylarh@yahoo.com>
2019-01-30 22:08:38 -0500

    Updated splash screen, fixed player hitbox, added super simple in game pause menu.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
 
     private bool pause_enabled = true;
+
+    public GameObject pause_menu;
+    public Text txt_0;
+    public Text txt_1;
 
     private GameObject player;
     private GameObject cam;
@@ -20,6 +25,17 @@ public class LevelManager : MonoBehaviour
 
     private float prev_tm;
     private int num_updates;
+
+    private int pause_cursor_pos = 0;
+
+    private float down_thresh = -0.8f;
+    private float up_thresh = 0.8f;
+    
+    private float max_rgb = 0.6f;
+    private float min_rgb = 0.0f;
+
+    private bool debouncing_dpad = true;
+    private bool debouncing_lstick = true;
 
     private const bool DEBUGGING = false;
 
@@ -53,24 +69,38 @@ public class LevelManager : MonoBehaviour
 
         // move the camera over the player so it doesn't have to lerp there
         this.cam.transform.position = new Vector3(this.player.transform.position.x, this.player.transform.position.y, this.cam.transform.position.z);
+
+        this.pause_menu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update(){
-        // input handling
-        if (QuitKeyPressed()){
-            SceneManager.LoadScene("MainMenu");
-        } else if (ResetKeyPressed()){
-            // load current scene name that has been saved
-            string scene_name = PlayerPrefs.GetString("current_scene", "");
-            SceneManager.LoadScene(scene_name);
-
-            // unpause if paused
-            if (this.paused){
+        if (this.paused){
+            if (PauseKeyPressed()){
+                TogglePause();
+            } else if (DownKeyPressed() || UpKeyPressed()){
+                this.pause_cursor_pos = (this.pause_cursor_pos + 1) % 2;
+                HandlePauseCursor();
+            } else if (GoKeyPressed()){
+                if (this.pause_cursor_pos == 0){
+                    TogglePause();
+                } else if (this.pause_cursor_pos == 1){
+                    TogglePause();
+                    SceneManager.LoadScene("MainMenu");
+                }
+            }
+        } else {
+            // input handling
+            if (QuitKeyPressed()){
+                TogglePause();
+                SceneManager.LoadScene("MainMenu");
+            } else if (ResetKeyPressed()){
+                // load current scene name that has been saved
+                string scene_name = PlayerPrefs.GetString("current_scene", "");
+                SceneManager.LoadScene(scene_name);
+            } else if (PauseKeyPressed()){
                 TogglePause();
             }
-        } else if (PauseKeyPressed()){
-            TogglePause();
         }
 
         // frame rate handling
@@ -82,6 +112,22 @@ public class LevelManager : MonoBehaviour
             this.num_updates = 0;
         } else {
             this.num_updates++;
+        }
+
+        if (this.debouncing_dpad){
+            if (Input.GetAxis("Vertical") < this.up_thresh && Input.GetAxis("Vertical") > this.down_thresh){
+                if (Input.GetAxis("Horizontal") < this.up_thresh && Input.GetAxis("Horizontal") > this.down_thresh){
+                    this.debouncing_dpad = false;
+                }
+            }
+        }
+        
+        if (this.debouncing_lstick){
+            if (Input.GetAxis("axy") < this.up_thresh && Input.GetAxis("axy") > this.down_thresh){
+                if (Input.GetAxis("axx") < this.up_thresh && Input.GetAxis("axx") > this.down_thresh){
+                    this.debouncing_lstick = false;
+                }
+            }
         }
     }
 
@@ -113,6 +159,38 @@ public class LevelManager : MonoBehaviour
             for (int i = 0; i < caulpers.Length; i++){
                 caulpers[i].SendMessage("Pause", this.paused);
             }
+            
+            this.pause_menu.SetActive(this.paused);
+
+            Color c = this.txt_0.color;
+            c.r = this.min_rgb;
+            c.g = this.min_rgb;
+            c.b = this.min_rgb;
+            this.txt_0.color = c;
+            this.txt_1.color = c;
+
+            HandlePauseCursor();
+        }
+    }
+
+    private void HandlePauseCursor(){
+        Color c = this.txt_0.color;
+        c.r = this.min_rgb;
+        c.g = this.min_rgb;
+        c.b = this.min_rgb;
+        this.txt_0.color = c;
+        this.txt_1.color = c;
+
+        if (this.pause_cursor_pos == 0){
+            c.r = this.max_rgb;
+            c.g = this.max_rgb;
+            c.b = this.max_rgb;
+            this.txt_0.color = c;
+        } else if (this.pause_cursor_pos == 1){
+            c.r = this.max_rgb;
+            c.g = this.max_rgb;
+            c.b = this.max_rgb;
+            this.txt_1.color = c;
         }
     }
 
@@ -126,5 +204,33 @@ public class LevelManager : MonoBehaviour
 
     bool PauseKeyPressed(){
         return Input.GetButtonDown("b7");
+    }
+    
+    bool GoKeyPressed(){
+        return Input.GetButtonDown("b0") || Input.GetKeyDown(KeyCode.Return);
+    }
+
+    bool DownKeyPressed(){
+        if (!this.debouncing_dpad && (Input.GetAxis("Vertical") < this.down_thresh || Input.GetAxis("Horizontal") > this.up_thresh)){
+            this.debouncing_dpad = true;
+            return true;
+        }
+        if (!this.debouncing_lstick && (Input.GetAxis("axy") > this.up_thresh || Input.GetAxis("axx") > this.up_thresh)){
+            this.debouncing_lstick = true;
+            return true;
+        }
+        return Input.GetKeyDown(KeyCode.DownArrow);
+    }
+    
+    bool UpKeyPressed(){
+        if (!this.debouncing_dpad && (Input.GetAxis("Vertical") > this.up_thresh || Input.GetAxis("Horizontal") < this.down_thresh)){
+            this.debouncing_dpad = true;
+            return true;
+        }
+        if (!this.debouncing_lstick && (Input.GetAxis("axy") < this.down_thresh || Input.GetAxis("axx") < this.down_thresh)){
+            this.debouncing_lstick = true;
+            return true;
+        }
+        return Input.GetKeyDown(KeyCode.UpArrow);
     }
 }
